04c1a85c5b2df2d677616a2aff0265851aa499e0
EnzoLsena <enzolevysena@gmail.com>
2026-06-23 20:03:24 -0300

    feat: add Docker setup with Dockerfile and docker-compose

--- Ecommerce.API/Program.cs
+++ Ecommerce.API/Program.cs
@@ -1,6 +1,4 @@
 using Ecommerce.Application;
-using Ecommerce.Infrastructure;
-
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
@@ -18,7 +16,7 @@ builder.Services.AddSwaggerGen(options =>
 });
 
 builder.Services.AddApplication();
-builder.Services.AddInfrastructure(builder.Configuration);
+
 
 var app = builder.Build();
 
